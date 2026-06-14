867a4535873259562ab1dbf5da49cbe02277e103
GODMYHELPER <dsomto166@gmail.com>
2026-06-14 17:42:09 +0100

    Finding Square Root

---
+++ Assignments/Finding Square Roots.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+print("Hi", name, ", let's ace this question.😁")
+
+number = int(input("First, What is the number you wish to find the square root? "))
+print("Okay give me a sec.......")
+print("Processing...", number)
+print("The answer to your question is", number ** 0.5)
+print("Enjoy your day, my dear", name)
