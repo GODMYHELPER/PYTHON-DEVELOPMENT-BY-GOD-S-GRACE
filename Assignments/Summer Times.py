1d549cab70984377e000d853734a689a318ea39e
GODMYHELPER <dsomto166@gmail.com>
2026-06-14 17:28:41 +0100

    Summer project(Assingment)

---
+++ Assignments/Summer Times.py
@@ -0,0 +1,7 @@
+temperature = int(input("What is the current temperature? "))
+print("Current temperature is:", temperature, "°C")
+if temperature < 30:
+    print("The weather is cold, put on a thick and hairy cloth")
+
+else:
+    print("The weather is hot, put on a light and soft cloth")
