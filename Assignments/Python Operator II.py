3d6ec8d31b9352894d840bcebee2fd7031042c20
GODMYHELPER <dsomto166@gmail.com>
2026-06-20 19:54:22 +0100

    Python Operator II

---
+++ Assignments/Python Operator II.py
@@ -0,0 +1,8 @@
+ch = input("Enter your desired character: ")
+print(ch)
+print("Processing.....")
+
+if "A" <= ch <= "Z" or "a" <= ch <= "z":
+    print("It is an alphabet, Thank you.")
+else:
+    print("It is not an alphabet, Thank you.")
